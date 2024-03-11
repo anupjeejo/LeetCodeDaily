c3a62f27972a28d4130d3c13de748ac1cb8a041f
anupjeejo <jeejoanup@gmail.com>
2024-03-12 01:20:09 +0530

    11-03-2024 solved problem optimized solution

--- 51. Custom-Sort-String.cpp
+++ 51. Custom-Sort-String.cpp
@@ -1,9 +1,37 @@
 class Solution {
 public:
+
+    void add(string &result, char ch, int count)
+    {
+        while(count>0){
+            result += ch;
+            count--;
+        }
+    }
+
     string customSortString(string order, string s) {
-        sort(s.begin(), s.end(), [order](char c1, char c2) { 
-            return order.find(c1) < order.find(c2);
-        });
-    return s;
+        unordered_map<char, int> mp;
+        string result;
+        int n = order.size();
+
+        for(auto ch : s)
+            mp[ch]++;
+
+        for(int i = 0; i<n; i++)
+        {
+            if(mp[order[i]] > 0){
+                add(result, order[i], mp[order[i]]);
+                mp[order[i]] = 0;
+            }
+        }
+
+        for(auto ele : mp)
+        {
+            if(ele.second > 0){
+                add(result, ele.first, ele.second);
+            }
+        }
+
+        return result;
     }
 };
