2b644847c8ad3504ce6a66c2142ba290df52e923
anupjeejo <jeejoanup@gmail.com>
2024-04-04 14:05:07 +0530

    4-04-2024 problem solved

---
+++ 59. Maximum-Nesting-Depth-of-the-Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n = s.size();
+        stack<char> st;
+        int result = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(s[i] == '(')
+…            }
+
+        }
+        return result;
+    }
+};
