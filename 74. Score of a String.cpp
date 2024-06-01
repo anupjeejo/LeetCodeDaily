5f7da612ae023f72025acdbfe0c2a88d1cf6c45f
anupjeejo <jeejoanup@gmail.com>
2024-06-01 12:09:30 +0530

    01-06-2024 problem solved

---
+++ 74. Score of a String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int result = 0;
+        int n = s.size();
+
+        for(int i=0; i<n-1; i++)
+            result += abs(s[i] - s[i+1]);
+
+        return result;        
+    }
+};
